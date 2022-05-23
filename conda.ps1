524e38b246da5e616fa9a9c29f5c2983251ded9e
Szymon Osiecki <szymonos@outlook.com>
2022-05-23 08:37:59 +0200

    repo: config

--- conda.ps1
+++ conda.ps1
@@ -4,42 +4,34 @@
 .SYNOPSIS
 Script for managing conda environments.
 .EXAMPLE
-./conda.ps1     # *Create/update environment
-./conda.ps1 -a  # *Activate environment
-./conda.ps1 -d  # *Deactivate environment
-./conda.ps1 -l  # *List packages
-./conda.ps1 -e  # *List environments
-./conda.ps1 -c  # *Clean conda
-./conda.ps1 -u  # *Update conda
-./conda.ps1 -r  # !Remove environment
+./conda.ps1 -o 'create'         # *Create/update environment
+./conda.ps1 -o 'activate'       # *Activate environment
+./conda.ps1 -o 'deactivate'     # *Deactivate environment
+./conda.ps1 -o 'packages'       # *List packages
+./conda.ps1 -o 'environments'   # *List environments
+./conda.ps1 -o 'update'         # *Update conda
+./conda.ps1 -o 'clean'          # *Clean conda
+./conda.ps1 -o 'remove'         # !Remove environment
+
+$CondaFile = 'environment.yml'
+./conda.ps1 -f $CondaFile -o 'create'   # *Create/update environment
+./conda.ps1 -f $CondaFile -o 'activate' # *Activate environment
+./conda.ps1 -f $CondaFile -o 'remove'   # !Remove environment
 #>
 [CmdletBinding()]
 param (
-    [Alias('a')][switch]$ActivateEnv,
-    [Alias('d')][switch]$DeactivateEnv,
-    [Alias('l')][switch]$ListPackages,
-    [Alias('e')][switch]$ListEnv,
-    [Alias('c')][switch]$CondaClean,
-    [Alias('u')][switch]$CondaUpdate,
-    [Alias('r')][switch]$RemoveEnv
+    [Alias('o')]
+    [Parameter(Mandatory)]
+    [ValidateSet('create', 'activate', 'deactivate', 'packages', 'environments', 'clean', 'update', 'remove')]
+    [string]$Option,
+
+    [Alias('f')]
+    [ValidateNotNullorEmpty()]
+    [string]$CondaFile = 'conda.yaml'
 )
 
-# const
-$ENV_FILE = 'conda.yaml'
-# calculate script variables
-$envName = (Select-String -Pattern '^name: +(\S+)' -Path $ENV_FILE).Matches.Groups[1].Value
-$isActivEnv = $null -ne $env:CONDA_DEFAULT_ENV -and -not $DeactivateEnv -and -not $RemoveEnv
-if (-not $PSBoundParameters.Count -or $RemoveEnv) {
-    $envExists = $envName -in (Get-CondaEnvironment).Name
-}
-
-# *Deactivate environment
-if (-not ($ListPackages -or $ListEnv)) {
-    Exit-CondaEnvironment
-}
-
 # *Check mamba installation
-if (-not $PSBoundParameters.Count -or $CondaClean -or $CondaUpdate) {
+if ($Option -in @('create', 'clean', 'update')) {
     $mamba = $env:CONDA_EXE -replace ('\bconda', 'mamba')
     if (-not (Test-Path $mamba)) {
         Write-Host 'mamba not found, installing...'
@@ -47,49 +39,73 @@ if (-not $PSBoundParameters.Count -or $CondaClean -or $CondaUpdate) {
     }
 }
 
-# *Create/update environment
-if (-not $PSBoundParameters.Count) {
-    if ($envExists) {
-        $msg = "`nEnvironment `e[1;4m$envName`e[0m already exists.`nProceed to update ([y]/n)?"
-        if ((Read-Host -Prompt $msg).ToLower() -in @('', 'y')) {
-            # update packages in existing environment
-            & $mamba env update --file $ENV_FILE --prune
-        } else {
-            Write-Host 'Done!'
-        }
-    } else {
-        Write-Host "`e[92mCreating `e[1;4m$envName`e[0;92m environment.`e[0m"
-        # create environment
-        & $mamba env create --file $ENV_FILE
+if ($Option -in @('create', 'activate', 'remove')) {
+    if (-not (Test-Path $CondaFile)) {
+        Write-Warning "File `e[4m$CondaFile`e[24m not found"
+        break
     }
-}
 
-# *List packages
-if ($ListPackages) {
-    Invoke-Conda list
-}
+    # get environment name
+    $envName = (Select-String -Pattern '^name: +(\S+)' -Path $CondaFile).Matches.Groups[1].Value
+    $envExists = $envName -in (Get-CondaEnvironment).Name 2>$null
 
-# *List environments
-if ($ListEnv) {
-    Invoke-Conda env list
-}
-
-# *Clean conda
-if ($CondaClean) {
-    & $mamba clean --all
-}
-
-# *Update conda
-if ($CondaUpdate) {
-    & $mamba update --name base --channel conda-forge --update-all
-}
-
-# *Remove environment
-if ($RemoveEnv -and $envExists) {
-    Invoke-Conda env remove --name $envName
-}
-
-# *Activate environment
-if (-not $PSBoundParameters.Count -or $ActivateEnv -or $isActivEnv) {
-    Enter-CondaEnvironment $envName
+    # exit environment before proceeding
+    Exit-CondaEnvironment
+    if ($envExists) {
+        switch ($Option) {
+            { $_ -eq 'create'} {
+                # *Create environment
+                Write-Host "`nEnvironment `e[1;4m$envName`e[22;24m already exist. Updating..."
+                & $mamba env update --file $CondaFile --prune 2>$null
+                Enter-CondaEnvironment $envName
+                break
+            }
+            { $_ -eq 'activate'} {
+                # *Activate environment
+                Enter-CondaEnvironment $envName
+                break
+            }
+            { $_ -eq 'remove'} {
+                # *Remove environment
+                Write-Host "Removing `e[1;4m$envName`e[22;24m environment."
+                Invoke-Conda env remove --name $envName 2>$null
+                break
+            }
+        }
+    } elseif ($Option -eq 'create') {
+        # *Update environment
+        Write-Host "Creating `e[1;4m$envName`e[22;24m environment."
+        & $mamba env create --file $CondaFile 2>$null
+        Enter-CondaEnvironment $envName
+    } else {
+        Write-Warning "`e[1;4m$envName`e[22;24m environment not found"
+    }
+} else {
+    switch ($Option) {
+        { $_ -eq 'deactivate'} {
+            # *Clean conda
+            Exit-CondaEnvironment
+            break
+        }
+        { $_ -eq 'packages'} {
+            # *List packages
+            Invoke-Conda list
+            break
+        }
+        { $_ -eq 'environments'} {
+            # *List environments
+            Invoke-Conda env list 2>$null
+            break
+        }
+        { $_ -eq 'update'} {
+            # *Update conda
+            & $mamba update --name base --channel conda-forge --update-all 2>$null
+            break
+        }
+        { $_ -eq 'clean'} {
+            # *Clean conda
+            & $mamba clean --all 2>$null
+            break
+        }
+    }
 }
