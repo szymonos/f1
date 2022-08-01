fadb27cbbac4a2c7aa0a26cc46cdde9036e925a0
Szymon Osiecki <szymonos@outlook.com>
2022-08-01 23:26:16 +0200

    refactor: conda script and env file

--- conda.ps1
+++ conda.ps1
@@ -3,8 +3,13 @@
 <#
 .SYNOPSIS
 Script for managing conda environments.
+.PARAMETER Option
+Select script action.
+.PARAMETER CondaFile
+Specify conda file to use.
+
 .EXAMPLE
-./conda.ps1 -o 'create'         # *Create/update environment
+./conda.ps1                     # *Create/update environment
 ./conda.ps1 -o 'activate'       # *Activate environment
 ./conda.ps1 -o 'deactivate'     # *Deactivate environment
 ./conda.ps1 -o 'packages'       # *List packages
@@ -13,105 +18,109 @@ Script for managing conda environments.
 ./conda.ps1 -o 'clean'          # *Clean conda
 ./conda.ps1 -o 'remove'         # !Remove environment
 
-$CondaFile = 'environment.yml'
-./conda.ps1 -f $CondaFile -o 'create'   # *Create/update environment
+$CondaFile = '.tmp/env.yml'
+./conda.ps1 -f $CondaFile               # *Create/update environment
 ./conda.ps1 -f $CondaFile -o 'activate' # *Activate environment
 ./conda.ps1 -f $CondaFile -o 'remove'   # !Remove environment
 #>
 [CmdletBinding()]
 param (
     [Alias('o')]
-    [Parameter(Mandatory)]
     [ValidateSet('create', 'activate', 'deactivate', 'packages', 'environments', 'clean', 'update', 'remove')]
-    [string]$Option,
+    [string]$Option = 'create',
 
     [Alias('f')]
     [ValidateNotNullorEmpty()]
     [string]$CondaFile = 'conda.yaml'
 )
 
-# *Check mamba installation
-if ($Option -in @('create', 'clean', 'update')) {
-    $mamba = $env:CONDA_EXE -replace ('\bconda', 'mamba')
-    if (-not (Test-Path $mamba)) {
-        Write-Host 'mamba not found, installing...'
-        Invoke-Conda install --name base --channel conda-forge mamba
-    }
-}
-
+# *Check for conda file
 if ($Option -in @('create', 'activate', 'remove')) {
-    if (-not (Test-Path $CondaFile)) {
+    if (Test-Path $CondaFile) {
+        # get environment name
+        $envName = (Select-String -Pattern '^name: +(\S+)' -Path $CondaFile).Matches.Groups[1].Value
+        $envExists = $envName -in (Get-CondaEnvironment).Name
+        # exit environment before proceeding
+        Exit-CondaEnvironment
+    } else {
         Write-Warning "File `e[4m$CondaFile`e[24m not found"
         break
     }
+}
 
-    # get environment name
-    $condaEnv = Get-Content -Path $CondaFile -Raw
-    $envName = (Select-String -Pattern '^name: +(\S+)' -InputObject $condaEnv).Matches.Groups[1].Value
-    $envExists = $envName -in (Get-CondaEnvironment).Name 2>$null
-
-    # exit environment before proceeding
-    Exit-CondaEnvironment
-    if ($envExists) {
-        switch ($Option) {
-            { $_ -eq 'create'} {
-                # *Create environment
-                Write-Host "`nEnvironment `e[1;4m$envName`e[22;24m already exist. Updating..."
-                & $mamba env update --file $CondaFile --prune 2>$null
-                Enter-CondaEnvironment $envName
-                break
-            }
-            { $_ -eq 'activate'} {
-                # *Activate environment
-                Enter-CondaEnvironment $envName
-                break
-            }
-            { $_ -eq 'remove'} {
-                # *Remove environment
-                Write-Host "Removing `e[1;4m$envName`e[22;24m environment."
-                Invoke-Conda env remove --name $envName 2>$null
-                break
-            }
+# *Execute option
+switch ($Option) {
+    'create' {
+        # check libmamba solver installation
+        if (-not (Get-ChildItem -Path "$env:_CONDA_ROOT/pkgs/" -Filter 'conda-libmamba-solver*' -Directory)) {
+            Write-Host 'conda-libmamba-solver not found, installing...'
+            Invoke-Conda install -y --name base --channel pkgs/main 'conda-libmamba-solver'
         }
-    } elseif ($Option -eq 'create') {
-        # *Update environment
-        Write-Host "Creating `e[1;4m$envName`e[22;24m environment."
-        & $mamba env create --file $CondaFile 2>$null
-        Enter-CondaEnvironment $envName
-
-        if (Select-String -Pattern 'pypath-magic' -InputObject $condaEnv) {
-            # add workspaceFolder to python path
-            pypath add $PWD 2>$null
+        if ($envExists) {
+            # *Create environment
+            Write-Host "`nEnvironment `e[1;4m$envName`e[22;24m already exist. Updating..."
+            $cmd = "conda env update --file $CondaFile --prune$($envName -eq 'base' ? '' : ' --experimental-solver=libmamba')"
+            Invoke-Expression $cmd
+            Enter-CondaEnvironment $envName
+        } else {
+            # *Update environment
+            Write-Host "Creating `e[1;4m$envName`e[22;24m environment."
+            conda env create --file $CondaFile --experimental-solver=libmamba
+            Enter-CondaEnvironment $envName
         }
-    } else {
-        Write-Warning "`e[1;4m$envName`e[22;24m environment not found"
+        break
     }
-} else {
-    switch ($Option) {
-        { $_ -eq 'deactivate'} {
-            # *Clean conda
-            Exit-CondaEnvironment
-            break
+
+    'activate' {
+        # *Activate environment
+        if ($envExists) {
+            Enter-CondaEnvironment $envName
+        } else {
+            Write-Host "`e[1;4m$envName`e[22;24m environment doesn't exist!"
         }
-        { $_ -eq 'packages'} {
-            # *List packages
-            Invoke-Conda list
-            break
+        break
+    }
+
+    'remove' {
+        # *Remove environment
+        if ($envName -eq 'base') {
+            Write-Host "Cannot remove `e[1;4mbase`e[22;24m environment!"
+        } elseif ($envExists) {
+            Write-Host "Removing `e[1;4m$envName`e[22;24m environment."
+            Invoke-Conda env remove --name $envName
+        } else {
+            Write-Host "`e[1;4m$envName`e[22;24m environment doesn't exist!"
         }
-        { $_ -eq 'environments'} {
-            # *List environments
-            Invoke-Conda env list 2>$null
-            break
-        }
-        { $_ -eq 'update'} {
-            # *Update conda
-            & $mamba update --name base --channel conda-forge --update-all 2>$null
-            break
-        }
-        { $_ -eq 'clean'} {
-            # *Clean conda
-            & $mamba clean --all 2>$null
-            break
-        }
+        break
+    }
+
+    'deactivate' {
+        # *Clean conda
+        Exit-CondaEnvironment
+        break
+    }
+
+    'packages' {
+        # *List packages
+        Invoke-Conda list
+        break
+    }
+
+    'environments' {
+        # *List environments
+        Invoke-Conda env list
+        break
+    }
+
+    'update' {
+        # *Update conda
+        conda update -y --name base --channel pkgs/main --update-all
+        break
+    }
+
+    'clean' {
+        # *Clean conda
+        conda clean -y --all
+        break
     }
 }
